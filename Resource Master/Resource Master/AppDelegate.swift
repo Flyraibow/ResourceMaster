871f77e80647c8aa65863e0919c9d4255036140f
Yujie Liu <yujiel@fb.com>
2018-02-05 18:11:58 -0800

    Init project

---
+++ Resource Master/Resource Master/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Resource Master
+//
+//  Created by Yujie Liu on 2/5/18.
+//  Copyright © 2018 Yujie Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Resource Master/Resource Master/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Resource Master
+//
+//  Created by Yujie Liu on 2/5/18.
+//  Copyright © 2018 Yujie Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
