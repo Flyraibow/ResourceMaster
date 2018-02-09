cd221cb7f3712e363c1bb1d95ac5a72d56948284
Yujie Liu <yujiel@fb.com>
2018-02-08 17:50:10 -0800

    Add file system view controller

---
+++ Resource Master/Resource Master/LeftScrollBar/FileSystemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LeftViewController.swift
+//  Resource Master
+//
+//  Created by Yujie Liu on 2/8/18.
+//  Copyright © 2018 Yujie Liu. All rights reserved.
+//
+
+import Cocoa
+
+class FileSystemViewController: NSViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
