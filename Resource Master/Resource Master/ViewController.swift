4d21e11a30a6ed0c874f7a7e29bbddd85f55901d
Yujie Liu <yujiel@fb.com>
2018-02-08 16:23:24 -0800

    clean project

--- Resource Master/Resource Master/ViewController.swift
+++ Resource Master/Resource Master/ViewController.swift
@@ -1,31 +1,28 @@
 //
 //  ViewController.swift
-//  SourceView
+//  TestSwift
 //
-//  Translated by OOPer in cooperation with shlab.jp, on 2015/10/24.
+//  Created by Yujie Liu on 2/8/18.
+//  Copyright © 2018 Yujie Liu. All rights reserved.
 //
-//
-/*
- Copyright (C) 2017 Apple Inc. All Rights Reserved.
- See LICENSE.txt for this sample’s licensing information
- 
- Abstract:
- View controller containing the lower UI controls and the embedded child view controller (split view controller).
- */
+
 import Cocoa
 
-@objc(ViewController)
 class ViewController: NSViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewWillAppear() {
-        super.viewWillAppear()
 
-    }
-    
-    deinit {
+  @IBOutlet weak var labPath: NSTextField!
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
     }
+  }
+
+
 }
+
